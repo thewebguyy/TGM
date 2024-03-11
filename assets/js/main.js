95fe14e9c4e8819fbcc4b35dbe9197aadd9df69f
Olabode <olabodewebdesigns02@gmail.com>
2024-03-12 00:02:39 +0100

    making changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,6 +152,13 @@
   /**
    * Preloader
    */
+
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
  
 
   /**
